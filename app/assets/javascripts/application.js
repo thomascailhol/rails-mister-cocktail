058fcebfa066ad46851c7283ce4e85aaab09ce54
Thomas Cailhol <cailhol.thomas@gmail.com>
2017-08-11 11:21:42 +0200

    integrated form + fixed destroy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to @dose.cocktail
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
